fe9c4cd74c751acdb555c900cbd08aed20c90dc3
surendra-karamsetty <surendra3207@gmail.com>
2026-02-21 15:45:16 +0530

    Commit changes

--- variables/r53.tf
+++ variables/r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "www" {
   count = 10  
   zone_id = var.zone_id
-  name    = "${instances[count.index]}.${var.name}" #mangodb.venkata.online
+  name    = "${var.instances[count.index]}.${var.name}" #mangodb.venkata.online
   type    = "A"
   ttl     = 1
   records = [aws_instance.Terraform[count.index].private_ip]
