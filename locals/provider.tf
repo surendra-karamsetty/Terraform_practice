77f049adef88b920767d5c583adc7096d34347cf
surendra-karamsetty <surendra3207@gmail.com>
2026-02-25 21:29:27 +0530

    commit changes

---
+++ locals/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket         = "venkata-state-aws-88s"
+    key            = "locals-demo.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
